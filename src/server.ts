cf4fa7fe4861eb521f4d3a5a82ad9ba19180ea2e
SifatU360 <sifatullah30.info@gmail.com>
2025-10-07 07:09:10 +0600

    feat: initialize healthcare server with Express and TypeScript
    - Added package.json with dependencies and scripts for development and testing.
    - Created Prisma schema for PostgreSQL database connection.
    - Implemented basic Express application with a health check endpoint.
    - Set up server entry point to start the application.
    - Configured TypeScript with strict type checking and output directory.

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { Server } from 'http';
+import app from './app'
+
+const port = 3000;
+
+// app.listen(port, ()=>{
+//     console.log("App is listening on port ", port);
+// })
+
+async function main(){
+    const server: Server = app.listen(port, () => {
+        console.log("Server is running on port ", port);
+    })
+}
+
+main();
