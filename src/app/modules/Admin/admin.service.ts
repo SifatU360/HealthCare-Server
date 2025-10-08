9aa1344c2141c90c5b3aff7a729cb7e7b5ba61ff
SifatU360 <sifatullah30.info@gmail.com>
2025-10-09 00:08:11 +0600

    feat: refactor admin data retrieval to use dynamic search conditions and improve query structure

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,24 +1,49 @@
-import { PrismaClient } from "../../../../generated/prisma";
+import { Prisma, PrismaClient } from "../../../../generated/prisma";
 
 const prisma = new PrismaClient();
 const getAllFromDB = async(params: any) => {
-    const result = await prisma.admin.findMany({
-        where: {
-            OR: [
-                {
-                    name: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
+    const andConditions: Prisma.AdminWhereInput[] = [];
+
+    // [
+    //             {
+    //                 name: {
+    //                     contains: params.searchTerm,
+    //                     mode: 'insensitive'
+    //                 }
+    //             },
+    //             {
+    //                 email: {
+    //                     contains: params.searchTerm,
+    //                     mode: 'insensitive'
+    //                 }
+    //             }
+    // ]
+    const {searchTerm, ...filterData} = params;
+    const adminSearchAbleFields = ['name', 'email'];
+    if(params.searchTerm){
+        andConditions.push({
+            OR: adminSearchAbleFields.map(field =>( {
+                [field]:{
+                    contains: params.searchTerm,
+                    mode: 'insensitive'
                 }
-            ]
-        }
+            }))
+        })
+    }
+
+    if(Object.keys(filterData).length>0){
+        andConditions.push({
+            AND: Object.keys(filterData).map(key => ({
+                [key] :{
+                    equals: filterData[key]
+                }
+            }))
+        })
+    }
+   
+    const whereConditions: Prisma.AdminWhereInput = {AND: andConditions};
+    const result = await prisma.admin.findMany({
+        where: whereConditions
     });
     return result;
 }
