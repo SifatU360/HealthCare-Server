073afcd50c32722e8baa6d2c8cb0005389b03909
SifatU360 <sifatullah30.info@gmail.com>
2025-10-14 11:03:54 +0600

    feat: refactor auth service for improved readability and consistency in formatting

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,81 +1,81 @@
 import { UserStatus } from "../../../../generated/prisma";
 import { jwtHelpers } from "../../../helpers/jwtHelpers";
-import prisma from "../../../shared/prisma"
-import bcrypt from 'bcrypt';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import prisma from "../../../shared/prisma";
+import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
+const loginUser = async (payload: { email: string; password: string }) => {
+  const userData = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
 
-    
-const loginUser = async(payload:{
-    email: string,
-    password: string
-}) =>{
-    const userData = await prisma.user.findUnique({
-        where: {
-            email: payload.email,
-            status: UserStatus.ACTIVE
-        }
-    });
+  const isCorrectPassword = await bcrypt.compare(
+    payload.password,
+    userData?.password as string
+  );
 
-    const isCorrectPassword = await bcrypt.compare(payload.password, userData?.password as string);
-    
-    if(!isCorrectPassword){
-        throw new Error("Password is incorrect");
-    }
-    const accessToken = jwtHelpers.generateToken({
-        email: userData?.email,
-        role: userData?.role
+  if (!isCorrectPassword) {
+    throw new Error("Password is incorrect");
+  }
+  const accessToken = jwtHelpers.generateToken(
+    {
+      email: userData?.email,
+      role: userData?.role,
     },
     "abcdefghl1046",
     "15m"
-    );
-    
-    const refreshToken = jwtHelpers.generateToken({
-        email: userData?.email,
-        role: userData?.role
+  );
+
+  const refreshToken = jwtHelpers.generateToken(
+    {
+      email: userData?.email,
+      role: userData?.role,
     },
     "abcdefghlloer3425",
     "30d"
-    );
-    console.log(accessToken);
+  );
+  console.log(accessToken);
 
-    return {
-        accessToken,
-        needPasswordChange: userData?.needPasswordChange,
-        refreshToken
-    };
-}
+  return {
+    accessToken,
+    needPasswordChange: userData?.needPasswordChange,
+    refreshToken,
+  };
+};
 
-const refreshToken = async(token: string) =>{
-    let decodedData;
-    try {
-      decodedData = jwtHelpers.verifyToken(token, "abcdefghlloer3425");
+const refreshToken = async (token: string) => {
+  let decodedData;
+  try {
+    decodedData = jwtHelpers.verifyToken(token, "abcdefghlloer3425");
+  } catch (error) {
+    throw new Error("You are not authorized user");
+  }
+  const isUserExist = await prisma.user.findUniqueOrThrow({
+    where: {
+      // email: (decodedData as any).email
+      email: decodedData.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
 
-    } catch (error) {
-        throw new Error("You are not authorized user");
-    }
-    const isUserExist = await prisma.user.findUniqueOrThrow({
-        where: {
-            // email: (decodedData as any).email
-            email: decodedData.email,
-            status: UserStatus.ACTIVE
-        }
-    });
-
-    const accessToken = jwtHelpers.generateToken({
-        email: isUserExist?.email,
-        role: isUserExist?.role
+  const accessToken = jwtHelpers.generateToken(
+    {
+      email: isUserExist?.email,
+      role: isUserExist?.role,
     },
     "abcdefghl1046",
     "15m"
-    );
-    return {
-        accessToken,
-        needPasswordChange: isUserExist?.needPasswordChange,
-    }; 
-}
+  );
+  return {
+    accessToken,
+    needPasswordChange: isUserExist?.needPasswordChange,
+  };
+};
 
 export const AuthService = {
-    loginUser,
-    refreshToken
-}
+  loginUser,
+  refreshToken,
+};
