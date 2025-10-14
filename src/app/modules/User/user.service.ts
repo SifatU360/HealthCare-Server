8cc9298b54cb41240a010541f08715b611f7ff31
SifatU360 <sifatullah30.info@gmail.com>
2025-10-14 20:36:02 +0600

    feat: update createAdmin function to enforce type safety and improve file upload handling

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,41 +1,42 @@
 import { fileURLToPathBuffer } from "node:url";
-import { PrismaClient, UserRole } from "../../../../generated/prisma";
+import { Admin, PrismaClient, UserRole } from "../../../../generated/prisma";
 import bcrypt from "bcrypt";
 import { fileUploader } from "../../../helpers/fileUploader";
+import { Request } from "express";
 const prisma = new PrismaClient();
 
-const createAdmin = async (req: any) => {
+const createAdmin = async (req: Request): Promise<Admin> => {
 
   const file = req.file;
 
   if(file){
-    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
-    console.log(uploadToCloudinary);
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file) as { secure_url: string };
+    req.body.admin.profilePhoto = uploadToCloudinary?.secure_url
   }
   
-  // const hashedPassword: string = await bcrypt.hash(data.password, 12);
-  // const userData = {
-  //   email: data.admin.email,
-  //   password: hashedPassword,
-  //   role: UserRole.ADMIN,
-  // };
+  const hashedPassword: string = await bcrypt.hash( req.body.password, 12);
+  const userData = {
+    email:  req.body.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  };
 
-  // const result = await prisma.$transaction(async (transactionClient) => {
-  //   const createdUserData = await transactionClient.user.create({
-  //     data: userData,
-  //   });
+  const result = await prisma.$transaction(async (transactionClient) => {
+    const createdUserData = await transactionClient.user.create({
+      data: userData,
+    });
 
-  //   const createdAdminData = await transactionClient.admin.create({
-  //     data: data.admin,
-  //   });
+    const createdAdminData = await transactionClient.admin.create({
+      data:  req.body.admin,
+    });
 
-  //   return createdAdminData;
-  // });
-  // // const adminData = {
-  // //     name: data.asmin.name,
-  // //     email : data.admin.email
-  // // }
-  // return result;
+    return createdAdminData;
+  });
+  // const adminData = {
+  //     name: data.asmin.name,
+  //     email : data.admin.email
+  // }
+  return result;
 };
 
 export const userService = {
