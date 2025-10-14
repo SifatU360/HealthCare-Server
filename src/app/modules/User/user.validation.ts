b3279efb3830fff09237d71401f309cc8c3134cf
SifatU360 <sifatullah30.info@gmail.com>
2025-10-14 22:38:01 +0600

    feat: add Doctor model and validation, update routes for creating Admin and Doctor

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { Gender } from "../../../../generated/prisma";
 
 const createAdmin = z.object({
   password: z.string({
@@ -17,6 +18,42 @@ const createAdmin = z.object({
   }),
 });
 
+const createDoctor = z.object({
+  password: z.string({
+    error: "Password is required",
+  }),
+  doctor: z.object({
+    name: z.string({
+      error: "Name is required!",
+    }),
+    email: z.string({
+      error: "Email is required!",
+    }),
+    contactNumber: z.string({
+      error: "Contact Number is required!",
+    }),
+    address: z.string().optional(),
+    registrationNumber: z.string({
+      error: "Registration Number is required!",
+    }),
+    experience: z.number().optional(),
+    gender: z.enum([Gender.MALE, Gender.FEMALE]),
+    appointmentFee: z.number({
+      error: "Appointment Fee is required!",
+    }),
+    qualification: z.string({
+      error: "Qualification is required!",
+    }),
+    currentWorkingPlace: z.string({
+      error: "Current Workplace is required!",
+    }),
+    designation: z.string({
+      error: "Designation is required!",
+    })
+  }),
+});
+
 export const userValidation = {
   createAdmin,
+  createDoctor
 };
--- src/helpers/fileUploader.ts
+++ src/helpers/fileUploader.ts
@@ -2,6 +2,7 @@ import multer from "multer"
 import path from "path"
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
+import { ICloudinaryResponse, IFile } from "../app/interfaces/file";
 
 //Configuration
 cloudinary.config({ 
@@ -24,15 +25,15 @@ const upload = multer({ storage: storage });
 
 
 
-const uploadToCloudinary = async(file: any) => {
+const uploadToCloudinary = async(file: IFile):Promise<ICloudinaryResponse> => {
 
     return new Promise((resolve, reject) => {
       cloudinary.uploader.upload(
         file.path,
-        {
-          public_id: file.originalname
-        },
-        (error, result) => {
+        // {
+        //   public_id: file.originalname
+        // },
+        (error:Error, result:ICloudinaryResponse) => {
           fs.unlinkSync(file.path)
           if(error){
             reject(error)
