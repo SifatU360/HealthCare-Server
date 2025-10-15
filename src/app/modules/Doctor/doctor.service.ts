68bd2d697535bad77588730b926aef92a39ab1f7
SifatU360 <sifatullah30.info@gmail.com>
2025-10-15 13:12:29 +0600

    Creating Doctor Specialties

--- src/app/modules/Doctor/doctor.service.ts
+++ src/app/modules/Doctor/doctor.service.ts
@@ -1,20 +1,38 @@
 import prisma from "../../../shared/prisma";
 
 const updateIntoDB = async (id: string, payload: any) => {
+  const { specialties, ...doctorData } = payload;
+
   const doctorInfo = await prisma.doctor.findUniqueOrThrow({
     where: {
       id,
     },
   });
 
-  const updatedDoctorInfo = await prisma.doctor.update({
-    where: {
-      id,
-    },
-    data: payload,
+  const result = await prisma.$transaction(async (transactionClent) => {
+    const updatedDoctorInfo = await transactionClent.doctor.update({
+      where: {
+        id,
+      },
+      data: doctorData,
+      include: {
+        doctorSpecialties: true,
+      },
+    });
+
+    for(const specialtiesId of specialties){
+      const createSpecialties = await transactionClent.doctorSpecialties.create({
+        data: {
+          doctorId: doctorInfo.id,
+          specialtiesId: specialtiesId
+        }
+      })
+    }
+
+    return updatedDoctorInfo
   });
 
-  return updatedDoctorInfo;
+  return result;
 };
 
 export const DoctorService = {
