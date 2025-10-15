3878bb9e974a44b02ffda2f1b26bde37c67ad777
SifatU360 <sifatullah30.info@gmail.com>
2025-10-16 00:49:04 +0600

    Preparing Data for Patient Health Data and Medical Report

--- src/app/modules/Patient/patient.service.ts
+++ src/app/modules/Patient/patient.service.ts
@@ -1,23 +1,26 @@
 import prisma from "../../../shared/prisma";
 
 const updateIntoDB = async (id: string, payload: any) => {
+  const { patientHealthData, medicalReport, ...patientData } = payload;
 
   const patientInfo = await prisma.patient.findUniqueOrThrow({
     where: {
-      id
+      id,
     },
   });
-  const result = await prisma.patient.update({
-    where: {
-        id
-    },
-    data: payload,
-    include: {
+  const result = await prisma.$transaction(async (transactionClient) => {
+    const result = await transactionClient.patient.update({
+      where: {
+        id,
+      },
+      data: patientData,
+      include: {
         patientHealthData: true,
-        medicalReport: true
-    }
-  })
-
+        medicalReport: true,
+      },
+    });
+    return result
+  });
 
   return result;
 };
