e2a1ce21c59d6185f4233649f7e6d71d1737245e
SifatU360 <sifatullah30.info@gmail.com>
2025-10-16 21:42:11 +0600

    Retrieving My Appointments as Doctor or Patient

--- src/app/modules/Appointment/appointment.controller.ts
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -4,6 +4,7 @@ import sendResponse from "../../../shared/sendResponse";
 import httpStatus from 'http-status'
 import { AppointmentService } from "./appointment.service";
 import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
 
 const createAppointment = catchAsync(async (req: Request & {user?: IAuthUser}, res: Response) => {
     
@@ -18,6 +19,36 @@ const createAppointment = catchAsync(async (req: Request & {user?: IAuthUser}, r
     })
 })
 
+const getMyAppointment = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const filters = pick(req.query, ['status', 'paymentStatus']);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+    const result = await AppointmentService.getMyAppointment(user as IAuthUser, filters, options);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'My Appointment retrive successfully',
+        data: result
+    });
+});
+
+// const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+//     const filters = pick(req.query, appointmentFilterableFields)
+//     const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+//     const result = await AppointmentService.getAllFromDB(filters, options);
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: 'Appointment retrieval successfully',
+//         meta: result.meta,
+//         data: result.data,
+//     });
+// });
+
+
 export const AppointmentController = {
-    createAppointment
+    createAppointment,
+    getMyAppointment
 }
