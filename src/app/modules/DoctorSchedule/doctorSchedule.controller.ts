3db1e059a451b3f9a7353b9a952001d746ef3778
SifatU360 <sifatullah30.info@gmail.com>
2025-10-16 14:31:32 +0600

    feat: Creating Schedule & Creating Doctor Schedule & Implementing Schedule Filtering & Retrieving Doctor Schedule with Filtering & Deleting Doctor Schedule

---
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { DoctorScheduleService } from "./doctorSchedule.service";
+import httpStatus from "http-status";
+import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
+
+const insertIntoDB = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const result = await DoctorScheduleService.insertIntoDB(user, req.body);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Doctor Schedule created successfully!",
+      data: result,
+    });
+  }
+);
+
+const getMySchedule = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const filters = pick(req.query, ["startDate", "endDate", "isBooked"]);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    const user = req.user;
+    const result = await DoctorScheduleService.getMySchedule(
+      filters,
+      options,
+      user as IAuthUser
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "My Schedule fetched successfully!",
+      data: result,
+    });
+  }
+);
+
+const deleteFromDB = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const {id} = req.params // scheduleId
+    const result = await DoctorScheduleService.deleteFromDB(user as IAuthUser, id);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "My Schedule deleted successfully!",
+      data: result,
+    });
+  }
+);
+
+export const DoctorScheduleController = {
+  insertIntoDB,
+  getMySchedule,
+  deleteFromDB,
+};
